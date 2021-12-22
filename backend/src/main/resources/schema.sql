76a19921c459ec1c5bc49f3544662a8e7327b3e2
Chiharu Miya <miya.chiharu@gmail.com>
2021-12-22 17:19:13 +0900

    CORS対応とスキーマ生成時に初期データを投入するようにした

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,3 +1,4 @@
 create table if not exists players (id serial primary key, first_name text, last_name text);
+delete from players;
 insert into players (first_name, last_name) values ('taro', 'yamada');
 insert into players (first_name, last_name) values ('jiro', 'sato');
